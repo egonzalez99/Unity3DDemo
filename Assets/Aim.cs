d96aa342b37eed51010a6b613c289239138d5eff
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2023-09-10 19:42:11 -0400

    Aim Script Updated

--- Assets/Aim.cs
+++ Assets/Aim.cs
@@ -8,10 +8,25 @@ public class Aim : MonoBehaviour
 
     private GameObject gun;
 
+    [SerializeField]
+
+    private GameObject playerHead;
+
+    private float rotationSpeed = 10.0f;
     private void Update()
     {
-        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        gun.transform.LookAt(target);
+        //Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //gun.transform.LookAt(target);
 
+        Vector3 direction = playerHead.transform.position - gun.transform.position;
+
+        // calculate the rotation
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // create a rotation
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // the gun will roatate towards the player
+        gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
